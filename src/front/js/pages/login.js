9a8049217fdcacc0a07972f241864f40dce112aa
Yvener Dutervil <72567854+yvener41@users.noreply.github.com>
2024-06-13 01:17:49 +0000

    update code

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,32 +1,62 @@
 import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
 export const Login = () => {
+	const navigate = useNavigate();
 	const { store, actions } = useContext(Context);
 	// 1. create 2 usestates, one for email one for password
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const token = sessionStorage.getItem("token");
+	
 
 	// 3. create an async function call handleclick that will include the fetch with options
 	// that includes the email and password in the body to be sent to the server as a post
+	const handleClick = () => {
+	    actions.login(email, password).then(() => {
+			navigate('/');
+		})
+	}
 
 	// 2. make the <input> controlled inputs
 	// make the button execute the handleclick function
 	return (
 		<div className="text-center mt-5">
+			{token && token !== "" && token !== undefined ?
+			<>
+			     <h1>You are logged in</h1>
+			</>
+           :
+			<>
 			<h1>Login</h1>
-
 			<div>
-                  <input type="text" placeholder="email" />
+                  <input 
+				  type="text" 
+				  placeholder="email"
+				  value={email}
+				  onChange={(e) => setEmail(e.target.value)} 
+				  />
 			</div>
 			   
 			<div>
-			    <input type="password" placeholder="password" />
+			    <input type="password" 
+				placeholder="password"
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+				 />
 			</div>
 
-			{/* onClick={HandleClick} */}
+			
 			<div>
-			    <button>Login</button>
+			    <button onClick={handleClick}>Login</button>
 			</div>
+			
+			
+			</>
+
+          }
 			
 		</div>
 	);
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -19,6 +20,52 @@ const getState = ({ getStore, getActions, setStore }) => {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
+			},
+                 //syncTokenFromSessionStore allows us to make the
+				 //session persist and avoid loosing the token on refresh
+			syncTokenFromSessionStore: () =>{
+				const sessionToken = sessionStorage.getItem('token');
+				console.log("Application just loaded, Syncing the sessionStorage token.")
+				if(sessionToken && sessionToken !=="" && sessionToken !== undefined){
+					setStore({token: sessionToken})
+				}
+			},
+
+				//we moved the log in function from login.js to here,
+				//creating an action in flux to handle the process
+				//and allowing us to set the token in the store for
+				//future use.
+			login: async (email, password) => {
+					const options = {
+						method: 'POST',
+						mode: 'cors',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify({
+							email: email,
+							password: password
+						}),
+					}
+			
+					const response = await fetch('https://legendary-space-potato-g6jg6775gp6cq4-3001.app.github.dev/api/token', options)
+
+					if(!response.ok){
+						console.log("error:", response.status, response.statusText)
+						return false;
+					}
+
+					const data = await response.json();
+					sessionStorage.setItem("token", data.access_token)
+					setStore({token: data.access_token})
+					return true;
+			},
+			
+              // logout allows removal of the token from the store and session storage
+			logout: () => {
+                sessionStorage.removeItem("token")
+				setStore({token: null})
+				console.log("You've logged out")
 			},
 
 			getMessage: async () => {
